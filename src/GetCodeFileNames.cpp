b7a2e2e0ca1efc25c4046ad4d42b60d329dcd9cb
bingozhu0197 <binbin.zhu@gmail.com>
2017-11-25 11:40:52 +0800

    refactor using c17 filesystem

--- src/GetCodeFileNames.cpp
+++ src/GetCodeFileNames.cpp
@@ -3,82 +3,41 @@
  *
  * Copyright (c) 2016 GPL. All rights reserved.
  *******************************************************************************/
-#include <unistd.h>
-#include <libgen.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <dirent.h>
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <experimental/filesystem>
 #include "Common.hpp"
 
-bool shouldIgnoreName(const string & p_fileName)
+namespace fs = std::experimental::filesystem;
+
+bool shouldIgnoreHideFiles(const string & p_fileName)
 {
     if (p_fileName.front() == '.')
     {
         return true;
     }
-    return false;    
+    return false;
 }
 
-bool isDirectory(const string & p_filePath)
+const vector<string> g_codeFileExtensions{".c", ".h", ".cpp", ".hpp", ".cc"};
+
+bool hasCodeFileExtension(const string & p_fileExtension)
 {
-    struct stat l_s;
-    if(stat(p_filePath.c_str(), &l_s) != -1
-       && S_ISDIR(l_s.st_mode))
-    {
-        return true;
-    }
-    return false;    
+    return std::find(std::begin(g_codeFileExtensions),
+              std::end(g_codeFileExtensions),
+              p_fileExtension) != std::end(g_codeFileExtensions);
 }
 
-bool isRegularFile(const string & p_filePath)
+bool isCodeFile(const auto & p_filePath)
 {
-    struct stat l_s;    
-    if(stat(p_filePath.c_str(), &l_s) != -1
-       && S_ISREG(l_s.st_mode))
+    if (fs::is_regular_file(p_filePath)
+        && hasCodeFileExtension(p_filePath.extension()))
     {
         return true;
     }
     return false;
-}
-
-const vector<string> CODE_FILE_POSTFIX{".c", ".h", ".cpp", ".hpp", ".cc"};
-
-bool hasCodeFilePostfix(const string & p_fileName)
-{
-    for (auto & l_postfix : CODE_FILE_POSTFIX)
-    {
-        if (p_fileName.length() > l_postfix.length()
-            && 0 == p_fileName.compare(p_fileName.length() - l_postfix.length(), l_postfix.length(), l_postfix))
-        {
-            return true;
-        }
-    }
-    return false;
-}
-    
-bool isCodeFile(const string & p_fileName)
-{
-    if (isRegularFile(p_fileName)
-        && hasCodeFilePostfix(p_fileName))
-    {
-        return true;
-    }
-    return false;
-}
-
-string buildFilePath(const string & p_directory,
-                     const string & p_fileName)
-{
-    if (p_directory.back() == '/')
-    {
-        return p_directory + p_fileName;
-    }
-    return p_directory + "/" +  p_fileName;
-
 }
 
 FilePaths operator += (FilePaths & lhs, FilePaths && rhs)
@@ -89,35 +48,30 @@ FilePaths operator += (FilePaths & lhs, FilePaths && rhs)
 
 FilePaths getCodeFilePathsRecursively(const string & p_directory)
 {
-    DIR * l_dir = opendir(p_directory.c_str());
-    if (l_dir == nullptr) 
+    if (!fs::is_directory(p_directory))
     {
         cerr << "can't open " << p_directory << endl;
         return {};
     }
-    
-    struct dirent *l_ent = nullptr;
+
     FilePaths l_filePaths;
-    while((l_ent = readdir (l_dir)) != nullptr)
+    for (auto & l_dirEntry : fs::directory_iterator(p_directory))
     {
-        string l_fileName = l_ent->d_name;
-        if (shouldIgnoreName(l_fileName))
+        auto & l_path = l_dirEntry.path();
+        if (shouldIgnoreHideFiles(l_path.filename()))
         {
             continue;
         }
 
-        string l_filePath = buildFilePath(p_directory, l_fileName);
-        if (isDirectory(l_filePath))
+        if (fs::is_directory(l_dirEntry))
         {
-            l_filePaths += getCodeFilePathsRecursively(l_filePath);
+            l_filePaths += getCodeFilePathsRecursively(l_dirEntry.path());
         }
-        else if (isCodeFile(l_filePath))
+        else if (isCodeFile(l_path))
         {
-            l_filePaths.push_back(make_shared<string>(l_filePath));
+            l_filePaths.push_back(make_shared<string>(l_path));
         }
     };
-    
-    closedir (l_dir);
+
     return l_filePaths;
 }
-
